f911882944768c6a5b33f7365529a898b19fec39
Simon Birchall <sbirchall@Simons-MacBook-Pro.local>
2017-04-01 19:25:09 +0100

    Refactor fileread to use callback

--- testscripts/main.js
+++ testscripts/main.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 
-var data = fs.readFileSync('testscripts/input.txt');
+fs.readFile('testscripts/input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
 
-console.log(data.toString());
 console.log("Program Ended");
